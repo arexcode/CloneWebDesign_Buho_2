08f5784c054608df6abb8e3575f9764128fde9cb
arexcode <rafaelcotrinadev@gmail.com>
2025-02-24 21:57:11 -0500

    feat: Agregar contenido al main de la pagina

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,35 @@ import { MainLayout } from "./layouts";
 function App(){
     return(
         <MainLayout>
-            <h2 className="text-center p-12"> Main </h2>
+
+            {/* Main Header */}
+            <div className="relative h-screen flex items-center justify-center text-white">
+                <div className="absolute inset-0 bg-[#005942] flex items-center justify-center">
+                    <div className="text-center max-w-3xl">
+                        <button className="px-12 py-2 bg-[#508c7c] text-white rounded-full opacity-80 mb-6">
+                            Chat software
+                        </button>
+                        <h1 className="text-[71px] font-bold leading-tight" style={{ fontFamily: "Poppins", fontWeight: 400 }}>
+                            Provide real-time support through chat for your customers
+                        </h1>
+                        <p className="mt-6 text-lg opacity-85" style={{ fontFamily: "Poppins" }}>
+                            Great software that allows you to chat from any place at any time without any interruption
+                        </p>
+
+                        <div className="mt-8 flex justify-center space-x-6">
+                            <button className="px-12 py-4 text-green-900 bg-yellow-400 text-black font-bold rounded-full hover:bg-yellow-300">
+                                Chatting Now
+                            </button>
+                            <button className="px-12 py-4 border border-yellow-400 text-yellow-400 font-semibold rounded-full hover:bg-yellow-400 hover:text-black">
+                                Learn More
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            
+
         </MainLayout>
     )
 }
